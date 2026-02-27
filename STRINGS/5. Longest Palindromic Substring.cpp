41f54fab7b29c850021dcdb0310081c99bf538e0
Himanshu <himanshukamishetti@gmail.com>
2026-02-27 19:58:01 +0530

    solved strings problems

---
+++ STRINGS/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+         int start = 0;
+        int maxLen = 1;
+
+        int n = s.length();
+        for (int i = 1; i < n; ++i) {
+            // Even length palindrome
+            int l = i - 1;
+            int r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                --l;
+                ++r;
+            }
+
+            // Odd length palindrome
+            l = i - 1;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                --l;
+                ++r;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
