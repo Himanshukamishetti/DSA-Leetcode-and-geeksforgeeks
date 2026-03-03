df8cfd1df21ddc4f12ddc347aaff506182f7e077
Himanshu <himanshukamishetti@gmail.com>
2026-03-03 14:50:00 +0530

    solved first non repeating character in a stream of characters problem

---
+++ QUEUE/First non-repeating character in a stream of characters.cpp
@@ -0,0 +1,25 @@
+string Solution::solve(string A) {
+    int n = A.length();
+    vector<int> vis(26,0);
+    string ans = "";
+    vector<char> v;
+    for(int i=0;i<n;i++){
+        if(!vis[A[i]-'a']){
+            v.push_back(A[i]);
+        }
+        vis[A[i]-'a']++;
+        int f=0;
+        int m = v.size();
+        for(int j=0;j<m;j++){
+            if(vis[v[j]-'a']==1){
+                ans.push_back(v[j]);
+                f=1;
+                break;
+            }
+        }
+        if(f==0){
+            ans.push_back('#');
+        }
+    }
+    return ans;
+}
